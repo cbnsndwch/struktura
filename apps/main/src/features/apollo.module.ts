5b21f50045866bc202406eeec8174b299613be9d
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2025-12-25 13:25:37 -0500

    refactor(main): migrate to custom nestjs apollo driver

--- apps/main/src/features/apollo.module.ts
+++ apps/main/src/features/apollo.module.ts
@@ -1,32 +1,17 @@
-import { ApolloServerPluginLandingPageLocalDefault as ApolloLandingPagePlugin } from '@apollo/server/plugin/landingPage/default';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
+
+import { ApolloDriver, ApolloDriverConfig } from '@cbnsndwch/nestjs-apollo';
 
 export const apolloModule = GraphQLModule.forRootAsync<ApolloDriverConfig>({
     driver: ApolloDriver,
     useFactory: async () => {
-        const shouldIncludePlayground =
-            process.env.GRAPHQL_PLAYGROUND !== 'false';
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const plugins: any[] = [];
-        if (shouldIncludePlayground) {
-            plugins.push(
-                ApolloLandingPagePlugin({
-                    footer: false,
-                    embed: true
-                })
-            );
-        }
+        const isNotProd = process.env.NODE_ENV !== 'production';
 
         return {
             cache: 'bounded',
             autoSchemaFile: true,
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            playground: 0 as any,
-            introspection: shouldIncludePlayground,
-            debug: shouldIncludePlayground,
-            plugins
+            introspection: isNotProd,
+            debug: isNotProd
         };
     }
 });
