15e2c6435f3fb47f39d2f3f6baf5faf9a44b9a3d
cbnsndwch <8313760+cbnsndwch@users.noreply.github.com>
2025-12-25 11:02:52 -0500

    refactor(main/routes): move ui-demo to examples folder

--- apps/main/app/examples/routes.ts
+++ apps/main/app/examples/routes.ts
@@ -1,5 +1,6 @@
 import { type RouteConfig, route } from '@react-router/dev/routes';
 
 export default [
-    route('examples/components', 'examples/components/page.tsx')
+    route('examples/components', 'examples/components/page.tsx'),
+    route('examples/ui-demo', 'examples/ui-demo/page.tsx')
 ] satisfies RouteConfig;
--- apps/main/app/routes/other/routes.ts
+++ apps/main/app/routes/other/routes.ts
@@ -2,6 +2,5 @@ import { route, type RouteConfig } from '@react-router/dev/routes';
 
 export default [
     route('favicon.ico', 'routes/other/favicon.ts'),
-    route('ui-demo', 'routes/other/ui-demo.tsx'),
     route('unauthorized', 'routes/other/unauthorized.tsx')
 ] satisfies RouteConfig;
